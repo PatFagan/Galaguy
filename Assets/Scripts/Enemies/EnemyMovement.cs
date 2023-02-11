4dd91765861cc6c9f863fff1c028e8b2d2a1008e
PatFagan <46632296+PatFagan@users.noreply.github.com>
2023-02-11 18:08:24 -0500

    player and enemy

---
+++ Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // set target loc by finding target object by tag
+    public void SetTarget(string targetTag)
+    {
+        GameObject target = GameObject.FindGameObjectWithTag(targetTag);
+        //navMeshAgent.SetDestination(target.transform.position);
+        print(transform.position);
+        print("player: " + target.transform.position);
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetTarget("Player");
+    }
+}
